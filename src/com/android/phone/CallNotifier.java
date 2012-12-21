29b2a9f983d861949ad0d5e5476d25ea970d4696
daedae <daeyoung1112@gmail.com>
2012-12-21 18:12:51 +0900

    Revert "call rec: prevent start of new recording during disconnection"
    This reverts commit 415e3e6ee0872daa12bc0a7349e0b35faf4dbc6d.

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -912,16 +912,12 @@ public class CallNotifier extends Handler
                         mInCallRingbackTonePlayer = null;
                     }
                     if (PhoneUtils.PhoneSettings.callRecordingEnabled(mApplication)) {
-                    if (callState == Call.State.ACTIVE &&
-                            callState != mLastCallState &&
-                            mLastCallState != Call.State.DISCONNECTING) {
-
+                        if (callState == Call.State.ACTIVE && callState != mLastCallState) {
                             log("onPhoneStateChanged: State ACTIVE - startRecording, last state was: " + mLastCallState);
                             Call call = PhoneUtils.getCurrentCall(fgPhone);
                             Connection c = PhoneUtils.getConnection(fgPhone, call);
                             PhoneUtils.startRecording(c.getAddress(), c.isIncoming() ? "in":"out");
-                    } else if (callState != Call.State.ACTIVE &&
-                            mLastCallState == Call.State.ACTIVE) {
+                        } else if (callState != Call.State.ACTIVE && mLastCallState == Call.State.ACTIVE) {
                             log("onPhoneStateChanged: State not ACTIVE - stopRecording");
                             PhoneUtils.stopRecording();
                         }
